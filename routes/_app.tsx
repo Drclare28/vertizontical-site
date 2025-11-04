c652fac3efc330c6e87d0c6c72089919d5dee64b
Ryan Clare <drclare2884@icloud.com>
2025-11-04 13:57:29 -0500

    favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default define.page(function App({ Component }) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Vertizontical Studios</title>
+        <link rel="icon" href="/VSicon.svg" type="image/svg+xml"></link>
       </head>
       <body class="pt-sans-regular bg-black">
         <Component />
