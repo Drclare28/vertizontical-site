84bf8a8ab4dec5e629983ec50ca4cbac29ffdf89
Ryan Clare <drclare2884@icloud.com>
2025-12-21 22:01:38 -0500

    facebook meta tag

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,6 +6,10 @@ export default define.page(function App({ Component }) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="facebook-domain-verification"
+          content="tiqws6d2bq5a9c8pniwi7g4l7jhtuv"
+        />
         <title>Vertizontical Studios</title>
         <link rel="icon" href="/VSicon.svg" type="image/svg+xml"></link>
       </head>
